fb2e316d134b5d72cd60245b8e78ff335a8991ab
kairken <157514879+kairken@users.noreply.github.com>
2024-01-25 17:43:33 +0600

    Create lists.py

---
+++ lab2/lists.py
@@ -0,0 +1,14 @@
+Ex.1
+fruits = ["apple", "banana", "cherry"]
+print(fruits[1])
+Ex.2
+fruits = ["apple", "banana", "cherry"]
+fruits[0]="kiwi"
+Ex.3
+fruits = ["apple", "banana", "cherry"]
+fruits.append("orange")
+Ex.4
+fruits = ["apple", "banana", "cherry"]
+fruits.insert(1,lemon)
+Ex.5
+fruits = ["apple", "banana", "cherry"]
